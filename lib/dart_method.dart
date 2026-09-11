361b3a2b5f5b9307732478e616fc64fffa0cac6d
Mudaser <mudaserrahman7@gmail.com>
2026-09-11 23:15:11 +0500

    even and odd

---
+++ lib/dart_method.dart
@@ -0,0 +1,12 @@
+void checkNumber(int number) {
+  if (number / 2 == 0) {
+    print('Number is Even');
+  } else {
+    print('Number is Odd');
+  }
+}
+
+void main() {
+  checkNumber(5);
+  checkNumber(6);
+}
