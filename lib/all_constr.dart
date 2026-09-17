b113ff61b787771a8ac39772ef53fab8d030b8ae
Mudaser <mudaserrahman7@gmail.com>
2026-09-17 22:42:36 +0500

    all constractor in one example

--- lib/all_constr.dart
+++ lib/all_constr.dart
@@ -3,11 +3,25 @@ class AllConstrctor {
   int? number;
   int? age;
   //Defult constructor
-  AllConstrctor() {
-    print("This is default constructor");
+  // ******************* default constructor *******************//
+  // AllConstrctor() {
+  //   print("This is default constructor");
+  // }
+  // ******************* parameterized constructor *******************//
+  AllConstrctor(String name, int number, int age) {
+    this.name = name;
+    this.number = number;
+    this.age = age;
   }
 }
 
 void main() {
-  AllConstrctor obj = AllConstrctor();
+  //*****************defult constructor *********************
+  //AllConstrctor obj = AllConstrctor();
+  //*****************parameterized constructor *********************
+  AllConstrctor obj = AllConstrctor("Mudaser", 1234567890, 25);
+  AllConstrctor obj1 = AllConstrctor('Ali', 9876543210, 30);
+  print("Name: ${obj.name} Number: ${obj.number} Age: ${obj.age}");
+
+  print("Name: ${obj1.name} Number: ${obj1.number} Age: ${obj1.age}");
 }
