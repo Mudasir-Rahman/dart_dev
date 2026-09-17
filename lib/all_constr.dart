7462d4b42f581bf8134034d14ca7707512e05a10
Mudaser <mudaserrahman7@gmail.com>
2026-09-17 22:36:40 +0500

    all constractor in one example

---
+++ lib/all_constr.dart
@@ -0,0 +1,13 @@
+class AllConstrctor {
+  String? name;
+  int? number;
+  int? age;
+  //Defult constructor
+  AllConstrctor() {
+    print("This is default constructor");
+  }
+}
+
+void main() {
+  AllConstrctor obj = AllConstrctor();
+}
